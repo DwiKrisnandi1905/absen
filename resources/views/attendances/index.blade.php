a624d34f7fcbb46f94e41fea11a9dccc674f4672
DwiKrisnandi1905 <116879948+DwiKrisnandi1905@users.noreply.github.com>
2023-12-20 00:07:38 +0700

    first commit

---
+++ resources/views/attendances/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@push('style')
+@powerGridStyles
+<link rel="stylesheet" href="{{ asset('css/button.css') }}">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
+@endpush
+
+@section('buttons')
+<div class="btn-toolbar mb-2 mb-md-0">
+    <div>
+        <a href="{{ route('attendances.create') }}" class="btn btn-sm btn-primary">
+            <span data-feather="plus-circle" class="align-text-bottom me-1"></span>
+            Tambah Data Absensi
+        </a>
+    </div>
+</div>
+@endsection
+
+@section('content')
+@include('partials.alerts')
+<livewire:attendance-table />
+@endsection
+
+@push('script')
+<script src="{{ asset('jquery/jquery-3.6.0.min.js') }}"></script>
+@powerGridScripts
+@endpush
